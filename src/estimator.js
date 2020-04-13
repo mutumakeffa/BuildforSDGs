322edabce66d5caf6087c70c57622842b883b6e6
Kefa Mutuma <mutmakeffa@gmail.com>
2020-04-13 22:25:23 +0300

    initial setup and build challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,31 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  const input = [
+    data : {
+      region: {
+        name: "Africa",
+        avgAge: 19.7,
+        avgDailyIncomeInUSD: 5,
+        avgDailyIncomePopulation: 0.71
+      },
+      periodType: "days",
+      timeToElapse: 58,
+      reportedCases: 674,
+      population: 66622705,
+      totalHospitalBeds: 1380614
+      
+    }
+  ];
 
+  return {
+    data: input,
+    impact: {
+      currentlyInfected : input.reportedCases * 10,
+      infectionsByRequestedTime : currentlyInfected * 512
+    },
+    severeImpact: {
+      currentlyInfected : input.reportedCases * 50,
+      infectionsByRequestedTime : currentlyInfected * 512
+    }
+  };
+}
 export default covid19ImpactEstimator;
